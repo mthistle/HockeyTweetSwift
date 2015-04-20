ec4d2c27d8d4101ea6ee86a5592dbb029235a5bb
Mark Thistle <mthistle@gmail.com>
2015-04-19 23:00:51 -0700

    Blue theme applied.

--- HockeyTweetSwift/ViewControllers/AboutViewController.swift
+++ HockeyTweetSwift/ViewControllers/AboutViewController.swift
@@ -10,4 +10,22 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        webView.loadHTMLString(self.webViewBody(), baseURL: nil)
+//        let buildVersion = NSBundle.mainBundle().infoDictionary!["CFBundleVersion"] as String
+//        let versionNumber = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"] as String
+//        self.versionLabel.text = "Version: \(versionNumber) (\(buildVersion))"
+    }
+
+    func webViewBody() -> String {
+        let bundle = NSBundle.mainBundle()
+        //let pathReachabilityLicense = bundle.pathForResource("Reachability_LICENSE", ofType: "txt")
+        //let ReachabilityLicense = String(contentsOfFile: pathReachabilityLicense!, encoding: NSUTF8StringEncoding, error: nil)
+        let body = "<html><body><h3>Licenses:</h3><ul><li></li></ul></body></html>"
+        return body
+    }
 }
